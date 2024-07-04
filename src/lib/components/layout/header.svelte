6fc46465d4cccf90a268c223c0ddb2e5d825d67f
trixky <mabois@student.42.fr>
2024-07-04 12:53:39 +0200

    [Fix] logo: fix the logo import

--- src/lib/components/layout/header.svelte
+++ src/lib/components/layout/header.svelte
@@ -1,11 +1,12 @@
 <!-- ================================================= SCRIPT -->
 <script lang="ts">
+  import { base } from '$app/paths';
 </script>
 
 <!-- ================================================= CONTENT -->
 <footer class="flex justify-center items-center p-10 mb-3 gap-5">
   <h1>smart_webcam</h1>
-  <img src="/favicon.png" width="55" alt="">
+  <img src={base + "/favicon.png"} width="55" alt="">
 </footer>
 
 <!-- ================================================= CSS -->
